62e60c522eb8edc3b4fd9e30c3fcc68a60ae01f9
TNDStudios <info@thenakeddeveloper.com>
2022-01-01 20:42:14 +0000

    Added Snailsort calling pattern

--- CodeKata.All/Katas/SnailSort.cs
+++ CodeKata.All/Katas/SnailSort.cs
@@ -1,44 +1,73 @@
 ﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Katas.SnailSort
 {
     /*
     Problem:
+
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    to
+        [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
     */
 
     #region Solution
-    public class Solution
+    public class SnailSorter
     {
+        public SnailSorter()
+        {
 
+        }
+
+        public string[] Sort(string[][] values)
+        {
+            return new string[] { };
+        }
     }
     #endregion
 
     #region Tests
     public class Tests
     {
-        [Fact]
-        public void Something_Does_Something_With_Thing_Expecting_SomethingElse()
+        public static string[][] SnailTest1 =
+            new string[][] {
+                new string[] { "1", "2", "3" },
+                new string[] { "4", "5", "6" },
+                new string[] { "7", "8", "9" }
+            };
+
+        public static string[] ExpectedResult1 = new string[] { "1", "2", "3", "6", "9", "8", "7", "4", "5" };
+
+        public static IEnumerable<object[]> GetData(int numTests)
         {
-            // ARRANGE
+            var allData = new List<object[]>
+            {
+                new object[] { SnailTest1, ExpectedResult1 }
+            };
 
-            // ACT
-
-            // ASSERT
+            return allData.Take(numTests);
         }
 
         [Theory]
-        [InlineData("Scenario 1")]
-        [InlineData("Scenario 2")]
-        [InlineData("Scenario 3")]
-        [InlineData("Scenario 4")]
-        public void Something_Does_SomethingSpecific_With_Thing_Expecting_SomethingElse(string scenario)
+        [MemberData(nameof(GetData), 1)]
+        public void Something_Does_SomethingSpecific_With_Thing_Expecting_SomethingElse(string[][] toSort, string[] sorted)
         {
             // ARRANGE
+            SnailSorter sorter = new SnailSorter();
 
             // ACT
+            string[] result = sorter.Sort(toSort);
 
             // ASSERT
+            result.Should().BeEquivalentTo(sorted);
         }
     }
     #endregion
