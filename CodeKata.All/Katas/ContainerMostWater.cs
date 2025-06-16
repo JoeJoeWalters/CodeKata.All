d50f9b72ff7a59b8efa4ba0ed2597770fe7bbd0b
Joe Walters <info@thenakeddeveloper.com>
2025-06-16 23:37:10 +0100

    Most Water solution

--- CodeKata.All/Katas/ContainerMostWater.cs
+++ CodeKata.All/Katas/ContainerMostWater.cs
@@ -1,20 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AwesomeAssertions;
+using System;
+using Xunit;
 
-namespace Katas
+namespace Katas.ContainerMostWater
 {
     /*
     https://leetcode.com/problems/container-with-most-water/
 
-https://dev.to/seanpgallivan/solution-container-with-most-water-1907
+    https://dev.to/seanpgallivan/solution-container-with-most-water-1907
 
-https://www.geeksforgeeks.org/container-with-most-water/
+    https://www.geeksforgeeks.org/container-with-most-water/
 
-https://leetcode.com/problems/deepest-leaves-sum/ 
+    https://leetcode.com/problems/deepest-leaves-sum/ 
     */
 
-    public class ContainerMostWater
+    #region Solution
+    public class Solution
     {
+        public Solution()
+        {
+
+        }
+
+        public int MaxArea(int[] heights)
+        {
+            if (heights == null || heights.Length < 2)
+                return 0; 
+            
+            int maxArea = 0;
+
+            for (var x = 0; x < heights.Length - 1; x++)
+            {
+                for (var next = x + 1; next < heights.Length; next++)
+                {
+                    // Calculate the area between the two markers
+                    int width = next - x;
+                    if (width <= 0)
+                        continue; // No area if width is zero or negative
+
+                    int height = Math.Min(heights[x], heights[next]); // Smaller of the two heights
+                    int area = width * height; 
+
+                    // Update maxArea if this is larger
+                    if (area > maxArea)
+                        maxArea = area;
+                }
+            }
+
+            return maxArea;
+        }
     }
+    #endregion
+
+    #region Tests
+    public class Tests
+    {
+        public Tests()
+        {
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [InlineData(new int[] { 1, 1 }, 1)]
+        public void Given_Markers_ShouldBeMaxSize(int[] heights, int expectedMaxArea)
+        {
+            // ARRANGE
+            var solution = new Solution();
+
+            // ACT
+            var maxArea = solution.MaxArea(heights);
+                
+            // ASSERT
+            maxArea.Should().Be(expectedMaxArea);
+
+        }
+    }
+
+    #endregion
 }
